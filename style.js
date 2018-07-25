555ddcc6ece850a8a55971c79f47fbe3ba7c76a1
DePaul <CONF0597@depaul.edu>
2018-07-25 10:57:35 -0500

    message

--- style.js
+++ style.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
 	
 	$("button").click(function(){
 	
-	var time =$("#time").val();
-	var verb =$("#verb").val();
-	var n1 =$("#n1").val();
-	var adj =$("#adj").val();
-	var n2 =$("#n2").val();
-	var n3 =$("#n3").val();
-	var e =$("#e").val();
+	var time1 =$("#time").val();
+	var verb1 =$("#verb").val();
+	var n11 =$("#n1").val();
+	var adj1 =$("#adj").val();
+	var n21 =$("#n2").val();
+	var n31 =$("#n3").val();
+	var e1 =$("#e").val();
 	
-	var mad ="One Valentine's "+time+". I was "+verb+", when I looked in my "+n1+" and saw a "+adj+" "+n2+"! It said, Will you be my "+n3+"? I was so "+e+"!";
+	var mad ="One Valentine's " +time1+ ". I was " +verb1+ ", when I looked in my " +n11+ " and saw a "+ adj1  +" "+ n21 +"! It said, 'Will you be my "+ n31 +"?' I was so "+ e1 +"!";
 	
 	console.log(mad);
-	$("text").text(mad);
+	$("text").html(mad);
 	});
 	
 	
